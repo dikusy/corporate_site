0613e60f8c8a90fc055bf4a4f540c4bc3a92e9d2
YumaUeno <y.ueno.1.1.1.5@gmail.com>
2019-06-11 01:23:49 +0900

    Add module css

--- src/components/memberCard/member.card.component.tsx
+++ src/components/memberCard/member.card.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Member } from '../../home/screen.component';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faGithub } from "@fortawesome/free-brands-svg-icons";
-import './style.css';
+import styles from './style.module.css';
 
 interface Props {
   member: Member;
@@ -10,23 +10,23 @@ interface Props {
 
 export const MemberCardComponent: React.SFC<Props> = (props) => {
   return (
-    <div className="member-card-wrapper">
-      <div className="member-card-image-wrapper">
-        <img className="member-card-image" src={ props.member.image } alt="member_image" width="100%" />
+    <div className={styles.container}>
+      <div className={styles.image_wrapper}>
+        <img className={styles.image} src={ props.member.image } alt="member_image" width="100%" />
       </div>
 
-      <div className="member-card-text-wrapper">
+      <div className={styles.text_wrapper}>
         <h4>{ props.member.name }</h4>
       </div>
 
-      <div className="self-introduction-wrapper">
+      <div className={styles.self_introduction_wrapper}>
         <p>{ props.member.text }</p>
       </div>
 
-      <div className="sns-icon-wrapper">
-        <FontAwesomeIcon className="sns-icon-logo" icon={faFacebook} />
-        <FontAwesomeIcon className="sns-icon-logo" icon={faTwitter} />
-        <FontAwesomeIcon className="sns-icon-logo" icon={faGithub} />
+      <div className={styles.sns_icon_wrapper}>
+        <FontAwesomeIcon className={styles.sns_icon_logo} icon={faFacebook} />
+        <FontAwesomeIcon className={styles.sns_icon_logo} icon={faTwitter} />
+        <FontAwesomeIcon className={styles.sns_icon_logo} icon={faGithub} />
       </div>
     </div>
   );
